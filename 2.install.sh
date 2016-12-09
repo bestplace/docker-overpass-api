de33e166b727742866eae3cdb8982af0012c869d
Dmitri Lebedev <ryba4@ryba4.com>
2016-12-09 23:11:21 +0300

    installation fixed

--- 2.install.sh
+++ 2.install.sh
@@ -1,4 +1,7 @@
-docker run -d -p 80:80 --name overpass_web -v "$(pwd)/planet":/usr/src/app/planet mediasuite/overpass-api:tst1
 
+sh 3.run.sh
 
 docker exec overpass_web /srv/osm3s/bin/init_osm3s.sh /usr/src/app/planet/planet.osm.bz2 /srv/osm3s/db /srv/osm3s --meta && rm -f /usr/src/app/planet/planet.osm.bz2
+
+docker exec overpass_web /etc/init.d/overpass stop
+docker exec overpass_web /etc/init.d/overpass start
